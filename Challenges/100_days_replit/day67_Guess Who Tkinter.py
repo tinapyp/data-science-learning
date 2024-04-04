38d1d53b80255921e48c03eceb28fa347edd2131
Fathin Afif <fathinafiff@gmail.com>
2024-04-04 18:29:35 +0700

    fix: image not showing

--- Challenges/100_days_replit/day67_Guess Who Tkinter.py
+++ Challenges/100_days_replit/day67_Guess Who Tkinter.py
@@ -6,15 +6,19 @@ root.title("Guess Who?!")
 
 
 def show_image():
-    global image
-    name = entry.get()
-    file_path = name + ".png"
-    if os.path.exists(file_path):
-        image = tk.PhotoImage(file=file_path)
-        image = image.subsample(5)
-        canvas.itemconfig(container, image=image)
-    else:
-        raise FileNotFoundError("File not found")
+    try:
+        global image
+        name = entry.get()
+        file_path = name + ".png"
+        if os.path.exists(file_path):
+            image = tk.PhotoImage(file=file_path)
+            image = image.subsample(5)
+            canvas.itemconfig(container, image=image)
+        else:
+            raise FileNotFoundError("File not found")
+    except Exception as e:
+        entry.delete(0, tk.END)
+        entry.insert(0, str(e))
 
 
 entry = tk.Entry(root)

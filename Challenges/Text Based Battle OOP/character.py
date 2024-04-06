0d4f2ddb49bf4543502e4b0adf169ef9f78a3733
Fathin Afif <fathinafiff@gmail.com>
2024-04-06 12:59:22 +0700

    add: Simple battle character OOP

--- Challenges/Text Based Battle OOP/character.py
+++ Challenges/Text Based Battle OOP/character.py
@@ -1,11 +1,43 @@
-class Character:
+from weapon import fists
+from health_bar import HealthBar
 
-    def __init__(self, name: str, health: int, damage: int) -> None:
+
+class Character:
+    is_alive = True
+
+    def __init__(self, name: str, health: int) -> None:
         self.name = name
         self.health = health
         self.health_max = health
-        self.damage = damage
+
+        self.weapon = fists
 
     def attack(self, target) -> None:
-        target.health -= self.damage
+        target.health -= self.weapon.damage
         target.health = max(target.health, 0)
+        target.health_bar.update()
+        print(
+            f"{self.name} dealt {self.weapon.damage} damage to {target.name} with {self.weapon.name}"
+        )
+
+
+class Hero(Character):
+    def __init__(self, name: str, health: int) -> None:
+        super().__init__(name, health)
+        self.default_weapon = self.weapon
+        self.health_bar = HealthBar(self, color="green")
+
+    def equip(self, weapon) -> None:
+        self.weapon = weapon
+        print(f"{self.name} equipped a(n) {self.weapon.name}!")
+
+    def drop(self) -> None:
+        print(f"{self.name} dropped the {self.weapon.name}!")
+        self.weapon = self.default_weapon
+
+
+class Enemy(Character):
+    def __init__(self, name: str, health: int, weapon) -> None:
+        super().__init__(name, health)
+        self.weapon = weapon
+        self.health_bar = HealthBar(self, color="red")
--- Challenges/Text Based Battle OOP/main.py
+++ Challenges/Text Based Battle OOP/main.py
@@ -1,13 +1,17 @@
-from character import Character
+from character import Hero, Enemy
+from weapon import short_bow, iron_sword
 
-hero = Character(name="Hero", health=100, damage=5)
-enemy = Character(name="Enemy", health=100, damage=3)
+hero = Hero(name="Hero", health=100)
+hero.equip(iron_sword)
+enemy = Enemy(name="Enemy", health=100, weapon=short_bow)
 
 while True:
     hero.attack(enemy)
     enemy.attack(hero)
 
-    print(f"Health of {hero.name}: {hero.health}")
-    print(f"Enemy of {enemy.name}: {enemy.health}")
+    hero.health_bar.draw()
+    enemy.health_bar.draw()
 
-    input()
+    if input("Next? (Y/n)") == "n":
+        print("Game End!")
+        break

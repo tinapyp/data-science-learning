e2d9b15cf443a06bb2f9255a9913776f7fd9cad8
Fathin Afif <fathinafiff@gmail.com>
2024-04-07 12:00:10 +0700

    add: passwrod with salt

--- Challenges/100_days_replit/day71_pass with salt.py
+++ Challenges/100_days_replit/day71_pass with salt.py
@@ -2,7 +2,7 @@ from replit import db
 import random
 
 
-def hash_password(password: str, salt: str) -> str:
+def hash_password(password: str, salt: str) -> int:
     hash_password = f"{password}{salt}"
     hash_password = hash(password)
     return hash_password

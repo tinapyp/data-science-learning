d1a1056bf4d751ac7d4331549fd3a0fa3135c068
Fathin Afif <fathinafiff@gmail.com>
2024-04-14 12:26:42 +0700

    add: reflection journal

--- Challenges/100_days_replit/day78_Reflection Jornal/app.py
+++ Challenges/100_days_replit/day78_Reflection Jornal/app.py
@@ -1,18 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
+@app.route("/")
+def home():
+    return redirect("/1")
+
+
 @app.route("/<pageNumber>")
-def main(pageNumber):
+def tree(pageNumber):
     thought = {"1": "Mudah sekali", "2": "Tidak sulit", "3": "Sangat simpel"}
     changeThought = thought.get(pageNumber)
-    with open("template/index.html", "r") as file:
-        print(pageNumber)
-        page = file.read()
-        page = page.replace("{pageNumber}", pageNumber)
-        page = page.replace("{list2}", str(changeThought))
-    return page
+    try:
+        with open("template/journal.html", "r") as file:
+            print(pageNumber)
+            page = file.read()
+            page = page.replace("{pageNumber}", pageNumber)
+            page = page.replace("{list2}", str(changeThought))
+        return page
+    except:
+        return redirect("/")
 
 
 app.run(debug="on")

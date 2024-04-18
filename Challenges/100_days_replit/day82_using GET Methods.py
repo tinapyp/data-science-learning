5c962f2d4169ff03bbf6e6bcfe64d8707e387623
Fathin Afif <fathinafiff@gmail.com>
2024-04-18 20:20:34 +0700

    add: GET method

--- Challenges/100_days_replit/day82_using GET Methods.py
+++ Challenges/100_days_replit/day82_using GET Methods.py
@@ -3,13 +3,15 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET"])
+@app.route("/", methods=["GET"])
 def index():
-  data = request.args
-  if data["lang"].lower() == "eng":
-    return "Hello, my friend from wordwide"
-  elif data['lang'].lower() == 'ind':
-    return "Halo, temanku dari dunia"
+    data = request.args
+    if data == {}:
+        return "Welcome to translator"
+    elif data["lang"].lower() == "eng":
+        return "Hello, my friend from wordwide"
+    elif data["lang"].lower() == "ind":
+        return "Halo, temanku dari dunia"
 
 
-app.run(host='0.0.0.0', port=81)
+app.run(host="0.0.0.0", port=81)
